d9944f93bd31e40230263b61f5b631e7ed615f37
jennihinck <69644871+jennihinck@users.noreply.github.com>
2020-10-08 16:55:51 -0500

    Update writingScript.js

--- js/writingScript.js
+++ js/writingScript.js
@@ -1,3 +1,12 @@
+function dropdownMenu() {
+	var x = document.getElementById('navMenu')
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
+};
+
 function menuCheck() {
 	if(window.innerWidth < 780) {
 		document.getElementById('navMenu').style.display = "none";
